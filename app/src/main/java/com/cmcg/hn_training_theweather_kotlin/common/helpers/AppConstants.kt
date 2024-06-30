a5620372d6d55e8f5bc364b7b230e5bdf8ee33cf
luongdt <luongdt@aht-asia.com>
2024-06-30 22:29:47 +0700

    init commit

---
+++ app/src/main/java/com/cmcg/hn_training_theweather_kotlin/common/helpers/AppConstants.kt
@@ -0,0 +1,8 @@
+package com.cmcg.the_weather.common.helpers
+
+class AppConstants {
+
+    companion object {
+        const val APP_NAME = "The Weather"
+    }
+}
---
+++ app/src/main/java/com/cmcg/hn_training_theweather_kotlin/common/helpers/AppHelpers.kt
@@ -0,0 +1,3 @@
+package com.cmcg.the_weather.common.helpers
+
+class AppHelpers {}
