2c7c51f46254f17e5e17a6e9f0881cf0de662901
cmc-Tony <vdthien1@cmcglobal.vn>
2024-07-01 15:09:09 +0700

    feat: add splash screen

---
+++ app/src/main/java/com/cmcg/hn_training_theweather_kotlin/HomeActivity.kt
@@ -0,0 +1,42 @@
+package com.cmcg.hn_training_theweather_kotlin
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.KeyEvent
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        supportActionBar?.show()
+    }
+
+    private var backPressedOnce = false
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.repeatCount == 0) {
+            event.startTracking()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.isTracking && !event.isCanceled) {
+            if (backPressedOnce) {
+                finish()
+            } else {
+                backPressedOnce = true
+                Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT).show()
+                Handler(Looper.getMainLooper()).postDelayed({ backPressedOnce = false }, 3000)
+                return true
+            }
+        }
+        return super.onKeyUp(keyCode, event)
+    }
+
+}
--- app/src/main/java/com/cmcg/hn_training_theweather_kotlin/MainActivity.kt
+++ app/src/main/java/com/cmcg/hn_training_theweather_kotlin/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.cmcg.hn_training_theweather_kotlin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.cmcg.hn_training_theweather_kotlin.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnGetStart.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
